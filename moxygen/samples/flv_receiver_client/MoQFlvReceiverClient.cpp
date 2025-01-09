4020968341977ea5670fad1cc874327572511679
Jordi Cenzano Ferret <jcenzano@meta.com>
2025-01-09 01:09:51 -0800

    Adding MoQFlvReceiverClient (from MOQT -> Video/Audio)
    Summary:
    # MoQFlvReceiverClient
    - Allows to receive s media stream in MOQT (MOQ-MI) and convert it to FLV (that can be stored on a file or sent to a pipe to play it with ffplay (or others), or do any other desired processing
    
    Reviewed By: afrind
    
    Differential Revision: D67638630
    
    fbshipit-source-id: 17c81431eafa3a303ef8c7b06404f9dcd99ca0ef

---
+++ moxygen/samples/flv_receiver_client/MoQFlvReceiverClient.cpp
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <folly/portability/GFlags.h>
+#include "moxygen/MoQClient.h"
+#include "moxygen/ObjectReceiver.h"
+
+#include <folly/init/Init.h>
+#include <folly/io/async/AsyncSignalHandler.h>
+#include <signal.h>
+
+DEFINE_string(
+    connect_url,
+    "https://localhost:4433/moq",
+    "URL for webtransport server");
+DEFINE_string(track_namespace, "flvstreamer", "Track Namespace");
+DEFINE_string(track_namespace_delimiter, "/", "Track Namespace Delimiter");
+DEFINE_string(video_track_name, "video0", "Video track Name");
+DEFINE_string(audio_track_name, "audio0", "Track Name");
+DEFINE_int32(connect_timeout, 1000, "Connect timeout (ms)");
+DEFINE_int32(transaction_timeout, 120, "Transaction timeout (s)");
+DEFINE_bool(quic_transport, false, "Use raw QUIC transport");
+DEFINE_bool(fetch, false, "Use fetch rather than subscribe");
+
+namespace {
+using namespace moxygen;
+
+class TrackType {
+ public:
+  enum MediaType { Audio, Video };
+
+  explicit TrackType(TrackType::MediaType mediaType) : mediaType_(mediaType) {}
+
+  std::string toStr() {
+    if (mediaType_ == TrackType::MediaType::Audio) {
+      return "audio";
+    } else if (mediaType_ == TrackType::MediaType::Video) {
+      return "video";
+    }
+    return "unknown";
+  }
+
+ private:
+  TrackType::MediaType mediaType_;
+};
+
+class TrackReceiverHandler : public ObjectReceiverCallback {
+ public:
+  explicit TrackReceiverHandler(TrackType::MediaType mediaType)
+      : trackMediaType_(TrackType(mediaType)) {}
+  ~TrackReceiverHandler() override = default;
+  FlowControlState onObject(const ObjectHeader&, Payload payload) override {
+    if (payload) {
+      std::cout << trackMediaType_.toStr() << " Received payload. Size="
+                << payload->computeChainDataLength() << std::endl;
+    }
+    return FlowControlState::UNBLOCKED;
+  }
+  void onObjectStatus(const ObjectHeader& objHeader) override {
+    std::cout << trackMediaType_.toStr()
+              << " ObjectStatus=" << uint32_t(objHeader.status) << std::endl;
+  }
+  void onEndOfStream() override {}
+  void onError(ResetStreamErrorCode error) override {
+    std::cout << trackMediaType_.toStr()
+              << " Stream Error=" << folly::to_underlying(error) << std::endl;
+  }
+  void onSubscribeDone(SubscribeDone) override {
+    baton.post();
+  }
+
+  folly::coro::Baton baton;
+
+ private:
+  TrackType trackMediaType_;
+};
+
+class MoQFlvReceiverClient {
+ public:
+  MoQFlvReceiverClient(folly::EventBase* evb, proxygen::URL url)
+      : moqClient_(
+            evb,
+            std::move(url),
+            (FLAGS_quic_transport
+                 ? MoQClient::TransportType::QUIC
+                 : MoQClient::TransportType::H3_WEBTRANSPORT)) {}
+
+  folly::coro::Task<void> run(
+      SubscribeRequest subAudio,
+      SubscribeRequest subVideo) noexcept {
+    XLOG(INFO) << __func__;
+    auto g =
+        folly::makeGuard([func = __func__] { XLOG(INFO) << "exit " << func; });
+    try {
+      co_await moqClient_.setupMoQSession(
+          std::chrono::milliseconds(FLAGS_connect_timeout),
+          std::chrono::seconds(FLAGS_transaction_timeout),
+          Role::SUBSCRIBER);
+      auto exec = co_await folly::coro::co_current_executor;
+      controlReadLoop().scheduleOn(exec).start();
+
+      // Subscribe to audio
+      subRxHandlerAudio_ = std::make_shared<ObjectReceiver>(
+          ObjectReceiver::SUBSCRIBE, &trackReceiverHandlerAudio_);
+      auto trackAudio = co_await moqClient_.moqSession_->subscribe(
+          subAudio, subRxHandlerAudio_);
+      if (trackAudio.hasValue()) {
+        subscribeIDAudio_ = trackAudio->subscribeID;
+        XLOG(DBG1) << "Audio subscribeID=" << subscribeIDAudio_;
+        auto latest = trackAudio->latest;
+        if (latest) {
+          XLOG(INFO) << "Audio Latest={" << latest->group << ", "
+                     << latest->object << "}";
+        }
+      } else {
+        XLOG(WARNING) << "Audio SubscribeError id="
+                      << trackAudio.error().subscribeID
+                      << " code=" << trackAudio.error().errorCode
+                      << " reason=" << trackAudio.error().reasonPhrase;
+      }
+
+      // Subscribe to video
+      subRxHandlerVideo_ = std::make_shared<ObjectReceiver>(
+          ObjectReceiver::SUBSCRIBE, &trackReceiverHandlerVideo_);
+      auto trackVideo = co_await moqClient_.moqSession_->subscribe(
+          subVideo, subRxHandlerVideo_);
+      if (trackVideo.hasValue()) {
+        subscribeIDVideo_ = trackVideo->subscribeID;
+        XLOG(DBG1) << "Video subscribeID=" << subscribeIDVideo_;
+        auto latest = trackVideo->latest;
+        if (latest) {
+          XLOG(INFO) << "Video Latest={" << latest->group << ", "
+                     << latest->object << "}";
+        }
+      } else {
+        XLOG(WARNING) << "Video SubscribeError id="
+                      << trackVideo.error().subscribeID
+                      << " code=" << trackVideo.error().errorCode
+                      << " reason=" << trackVideo.error().reasonPhrase;
+      }
+      moqClient_.moqSession_->drain();
+    } catch (const std::exception& ex) {
+      XLOG(ERR) << ex.what();
+      co_return;
+    }
+    co_await trackReceiverHandlerAudio_.baton;
+    XLOG(INFO) << __func__ << " done";
+  }
+
+  void stop() {
+    moqClient_.moqSession_->unsubscribe({subscribeIDAudio_});
+    moqClient_.moqSession_->unsubscribe({subscribeIDVideo_});
+    moqClient_.moqSession_->close(SessionCloseErrorCode::NO_ERROR);
+  }
+
+  folly::coro::Task<void> controlReadLoop() {
+    class ControlVisitor : public MoQSession::ControlVisitor {
+     public:
+      explicit ControlVisitor(MoQFlvReceiverClient& client) : client_(client) {}
+
+      void operator()(Announce announce) const override {
+        XLOG(WARN) << "Announce ns=" << announce.trackNamespace;
+        // text client doesn't expect server or relay to announce anything,
+        // but announce OK anyways
+        client_.moqClient_.moqSession_->announceOk({announce.trackNamespace});
+      }
+
+      void operator()(SubscribeRequest subscribeReq) const override {
+        XLOG(INFO) << "SubscribeRequest";
+        client_.moqClient_.moqSession_->subscribeError(
+            {subscribeReq.subscribeID, 404, "don't care"});
+      }
+
+      void operator()(Goaway) const override {
+        XLOG(INFO) << "Goaway";
+        client_.moqClient_.moqSession_->unsubscribe(
+            {client_.subscribeIDAudio_});
+        client_.moqClient_.moqSession_->unsubscribe(
+            {client_.subscribeIDVideo_});
+      }
+
+     private:
+      MoQFlvReceiverClient& client_;
+    };
+    XLOG(INFO) << __func__;
+    auto g =
+        folly::makeGuard([func = __func__] { XLOG(INFO) << "exit " << func; });
+    ControlVisitor visitor(*this);
+    MoQSession::ControlVisitor* vptr(&visitor);
+    while (auto msg =
+               co_await moqClient_.moqSession_->controlMessages().next()) {
+      boost::apply_visitor(*vptr, msg.value());
+    }
+  }
+
+ private:
+  MoQClient moqClient_;
+  SubscribeID subscribeIDAudio_{0};
+  SubscribeID subscribeIDVideo_{0};
+  TrackReceiverHandler trackReceiverHandlerAudio_ =
+      TrackReceiverHandler(TrackType::MediaType::Audio);
+  std::shared_ptr<ObjectReceiver> subRxHandlerAudio_;
+  TrackReceiverHandler trackReceiverHandlerVideo_ =
+      TrackReceiverHandler(TrackType::MediaType::Video);
+  std::shared_ptr<ObjectReceiver> subRxHandlerVideo_;
+};
+} // namespace
+
+using namespace moxygen;
+
+int main(int argc, char* argv[]) {
+  folly::Init init(&argc, &argv, false);
+  folly::EventBase eventBase;
+  proxygen::URL url(FLAGS_connect_url);
+  if (!url.isValid() || !url.hasHost()) {
+    XLOG(ERR) << "Invalid url: " << FLAGS_connect_url;
+  }
+
+  XLOGF(INFO, "Starting consumer from URL: {}", FLAGS_connect_url);
+
+  MoQFlvReceiverClient flvReceiverClient(&eventBase, std::move(url));
+
+  class SigHandler : public folly::AsyncSignalHandler {
+   public:
+    explicit SigHandler(folly::EventBase* evb, std::function<void(int)> fn)
+        : folly::AsyncSignalHandler(evb), fn_(std::move(fn)) {
+      registerSignalHandler(SIGTERM);
+      registerSignalHandler(SIGINT);
+    }
+    void signalReceived(int signum) noexcept override {
+      fn_(signum);
+      unreg();
+    }
+
+    void unreg() {
+      unregisterSignalHandler(SIGTERM);
+      unregisterSignalHandler(SIGINT);
+    }
+
+   private:
+    std::function<void(int)> fn_;
+  };
+  SigHandler handler(&eventBase, [&flvReceiverClient](int) mutable {
+    flvReceiverClient.stop();
+  });
+  const auto subscribeIDAudio = 0;
+  const auto trackAliasAudio = 1;
+  const auto subscribeIDVideo = 1;
+  const auto trackAliasVideo = 2;
+  flvReceiverClient
+      .run(
+          {subscribeIDAudio,
+           trackAliasAudio,
+           moxygen::FullTrackName(
+               {{TrackNamespace(
+                    FLAGS_track_namespace, FLAGS_track_namespace_delimiter)},
+                FLAGS_audio_track_name}),
+           0,
+           GroupOrder::OldestFirst,
+           LocationType::LatestObject,
+           folly::none,
+           folly::none,
+           {}},
+          {subscribeIDVideo,
+           trackAliasVideo,
+           moxygen::FullTrackName(
+               {TrackNamespace(
+                    FLAGS_track_namespace, FLAGS_track_namespace_delimiter),
+                FLAGS_video_track_name}),
+           0,
+           GroupOrder::OldestFirst,
+           LocationType::LatestObject,
+           folly::none,
+           folly::none,
+           {}})
+      .scheduleOn(&eventBase)
+      .start()
+      .via(&eventBase)
+      .thenTry([&handler](auto) { handler.unreg(); });
+  eventBase.loop();
+}
