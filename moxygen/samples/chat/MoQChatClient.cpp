b1f485272c3c6963925dd209c755768d6abf25b4
Alan Frindell <afrind@meta.com>
2025-02-22 18:03:08 -0800

    Subscribe state management
    Summary:
    1) Updated SubscribeDoneStatusCodes (removed UNSUBSCRIBED)
    2) Now no subscribeDone() is required by a publisher if the consumer invokes unsubscribe()
    3) When publisher receives unsubscribe, it will reset open streams
    4) When subscriber sends unsubscribe, it immediately removes state (callback will not get subscribeDone()
    
    Differential Revision: D69935505

--- moxygen/samples/chat/MoQChatClient.cpp
+++ moxygen/samples/chat/MoQChatClient.cpp
@@ -139,15 +139,7 @@ folly::coro::Task<Publisher::SubscribeResult> MoQChatClient::subscribe(
 void MoQChatClient::unsubscribe() {
   // MoQChatClient only publishes a single track at a time.
   XLOG(INFO) << "Unsubscribe id=" << *chatSubscribeID_;
-  if (publisher_) {
-    publisher_->subscribeDone(
-        {*chatSubscribeID_,
-         SubscribeDoneStatusCode::UNSUBSCRIBED,
-         0, // filled in by session
-         "",
-         folly::none});
-    publisher_.reset();
-  }
+  publisher_.reset();
   chatSubscribeID_.reset();
   chatTrackAlias_.reset();
 }
@@ -324,8 +316,7 @@ void MoQChatClient::subscribeDone(SubscribeDone subDone) {
          userTrackIt != userTracks.second.end();
          ++userTrackIt) {
       if (userTrackIt->subscribeId == subDone.subscribeID) {
-        if (subDone.statusCode != SubscribeDoneStatusCode::UNSUBSCRIBED &&
-            userTrackIt->subscription) {
+        if (userTrackIt->subscription) {
           userTrackIt->subscription->unsubscribe();
         }
         userTracks.second.erase(userTrackIt);
