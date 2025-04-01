21e7169e82144ea90157d366bfc2ccee6bd74e9a
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2025-04-01 04:58:41 -0700

    Revert D72157566
    Summary:
    This diff reverts D72157566
    This shouldn't have been landed the way it was to begin with
    The googletest upgrade is important and the issues were already being handled and fixed
    
    Reviewed By: hershi
    
    Differential Revision: D72224956
    
    fbshipit-source-id: 3c3ea73f9d4133a6c7280f63658fea6a3cf381ed

--- moxygen/test/TestHelpers.h
+++ moxygen/test/TestHelpers.h
@@ -29,7 +29,7 @@
    private:                                                                    \
     void TestBody() override;                                                  \
     folly::coro::Task<void> co_TestBody();                                     \
-    static ::testing::TestInfo* const test_info_ GTEST_ATTRIBUTE_UNUSED_;      \
+    static ::testing::TestInfo* const test_info_ [[maybe_unused]];             \
   };                                                                           \
                                                                                \
   ::testing::TestInfo* const GTEST_TEST_CLASS_NAME_(                           \
@@ -90,7 +90,7 @@
               ::testing::internal::CodeLocation(__FILE__, __LINE__));          \
       return 0;                                                                \
     }                                                                          \
-    static int gtest_registering_dummy_ GTEST_ATTRIBUTE_UNUSED_;               \
+    static int gtest_registering_dummy_ [[maybe_unused]];                      \
     GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)                         \
     (const GTEST_TEST_CLASS_NAME_(test_suite_name, test_name) &) = delete;     \
     GTEST_TEST_CLASS_NAME_(test_suite_name, test_name) & operator=(            \
