9b388e579b74e96d51831f634496695d2ed6c219
Alan Frindell <afrind@meta.com>
2025-11-12 18:45:20 -0800

    Make a pass through TrackConsumerFilter
    Summary: as it title
    
    Reviewed By: sandarsh
    
    Differential Revision: D86833148
    
    fbshipit-source-id: d00ce022b82819e815867c7c6dfdd1ba1c557c39

---
+++ moxygen/MoQFilters.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <moxygen/MoQConsumers.h>
+
+namespace moxygen {
+class TrackConsumerFilter : public TrackConsumer {
+ public:
+  explicit TrackConsumerFilter(std::shared_ptr<TrackConsumer> downstream)
+      : downstream_(std::move(downstream)) {}
+
+  folly::Expected<folly::Unit, MoQPublishError> setTrackAlias(
+      TrackAlias alias) override {
+    return downstream_->setTrackAlias(alias);
+  }
+
+  folly::Expected<std::shared_ptr<SubgroupConsumer>, MoQPublishError>
+  beginSubgroup(uint64_t groupID, uint64_t subgroupID, Priority priority)
+      override {
+    return downstream_->beginSubgroup(groupID, subgroupID, priority);
+  }
+
+  folly::Expected<folly::SemiFuture<folly::Unit>, MoQPublishError>
+  awaitStreamCredit() override {
+    return downstream_->awaitStreamCredit();
+  }
+
+  folly::Expected<folly::Unit, MoQPublishError> objectStream(
+      const ObjectHeader& header,
+      Payload payload) override {
+    return downstream_->objectStream(header, std::move(payload));
+  }
+
+  folly::Expected<folly::Unit, MoQPublishError> datagram(
+      const ObjectHeader& header,
+      Payload payload) override {
+    return downstream_->datagram(header, std::move(payload));
+  }
+
+  folly::Expected<folly::Unit, MoQPublishError> groupNotExists(
+      uint64_t groupID,
+      uint64_t subgroup,
+      Priority pri,
+      Extensions extensions) override {
+    return downstream_->groupNotExists(
+        groupID, subgroup, pri, std::move(extensions));
+  }
+
+  folly::Expected<folly::Unit, MoQPublishError> subscribeDone(
+      SubscribeDone subDone) override {
+    return downstream_->subscribeDone(std::move(subDone));
+  }
+
+  void setDeliveryCallback(
+      std::shared_ptr<DeliveryCallback> callback) override {
+    downstream_->setDeliveryCallback(std::move(callback));
+  }
+
+ private:
+  std::shared_ptr<TrackConsumer> downstream_;
+};
+
+} // namespace moxygen
