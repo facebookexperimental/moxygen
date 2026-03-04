0836db74740aeca8df4d5b8ade58eb5ecd7dcbc4
Alan Frindell <afrind@meta.com>
2026-03-04 15:19:41 -0800

    Relicense moxygen to Apache 2.0 (header codemod)
    Summary:
    Update all source file license headers from MIT/BSD to Apache 2.0
    as part of relicensing moxygen per T254257593.
    
    Includes: C++ headers/sources, Python scripts, CMake files, shell scripts.
    Also updates generate_cmake.py fallback header and regenerates
    auto-generated CMakeLists.txt files.
    
    Reviewed By: sandarsh
    
    Differential Revision: D95101372
    
    fbshipit-source-id: 8f16a09f368f8bc4f396f954f2aea4b28012f8e8

--- moxygen/mlog/MLogTypes.cpp
+++ moxygen/mlog/MLogTypes.cpp
@@ -1,9 +1,7 @@
 /*
- *  Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #include "moxygen/mlog/MLogTypes.h"
--- standalone/install-system-deps.sh
+++ standalone/install-system-deps.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
+# This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 
 # Installs system dependencies required for standalone moxygen build
